0b74c5d84778581313633babe27e6bd5fdd16411
Crowdbotics <team@crowdbotics.com>
2021-09-13 00:20:47 +0000

    Update models

--- backend/tunedin_30489/wsgi.py
+++ backend/tunedin_30489/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tunedin_30489.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tunedin_30489.settings")
 
 application = get_wsgi_application()
