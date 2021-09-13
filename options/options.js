1526a84a4c1079ff45cacff72e9c49d03c183d50
Crowdbotics <team@crowdbotics.com>
2021-09-13 00:15:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tunedin_30489Identifier",
+  url: "https://tunedin_30489Identifier.botics.co",
+  api: "https://tunedin_30489Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
